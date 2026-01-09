fc60ba748ad68954d2b47e34bc1d5ca8d0b68fa9
dbdev72 <dbdev1721@gmail.com>
2026-01-09 12:44:56 +0100

    fix: sincronització final de versions gradle 8.9.1

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,27 +1,7 @@
-pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
-        }
-
-    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
-
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.2.1" apply false 
-    // Pugem a la versió 2.1.0 per ser compatibles amb AdMob 24.9.0
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application") version "8.9.1" apply false 
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
 }
 
 include(":app")
