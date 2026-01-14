a9480f70c8e6f8daebf946c7994c20dea1043337
dbdev72 <dbdev1721@gmail.com>
2026-01-14 15:02:49 +0100

    fix: recuperat flutter.jar i eliminats enllaços manuals de plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,11 +57,24 @@ flutter {
 }
 
 dependencies {
-
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-common-java8:2.8.7")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.15.0")
+
+    // Aquest bloc és el que necessita per trobar 'io.flutter' i 'FlutterActivity'
+    val properties = Properties()
+    val propertiesFile = rootProject.file("local.properties")
+    if (propertiesFile.exists()) {
+        propertiesFile.inputStream().use { properties.load(it) }
+    }
+    val flutterSdkPath = properties.getProperty("flutter.sdk") ?: ""
+    
+    if (flutterSdkPath.isNotEmpty()) {
+        // Això soluciona els errors de "package io.flutter... does not exist"
+        compileOnly(files("$flutterSdkPath/bin/cache/artifacts/engine/android-arm64/flutter.jar"))
+        implementation(files("$flutterSdkPath/bin/cache/artifacts/engine/android-arm64/flutter.jar"))
+    }
 }
